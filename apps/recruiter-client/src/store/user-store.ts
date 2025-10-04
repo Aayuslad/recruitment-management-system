2f38f2db255d989f9c272ec75c258b1705e8b41a
aayush <aayushlad058@gmail.com>
2025-10-04 22:54:23 +0530

    fix: lint error

--- apps/recruiter-client/src/store/user-store.ts
+++ apps/recruiter-client/src/store/user-store.ts
@@ -3,11 +3,4 @@ import type { StateCreator } from 'zustand';
 // eslint-disable-next-line
 export type UserStoreSliceType = {};
 
-export const createUserSlice: StateCreator<UserStoreSliceType> = (
-    //@ts-expect-error
-    // eslint-disable-next-line 
-    set,
-    //@ts-expect-error
-    // eslint-disable-next-line
-    get 
-) => ({});
+export const createUserSlice: StateCreator<UserStoreSliceType> = () => ({});
